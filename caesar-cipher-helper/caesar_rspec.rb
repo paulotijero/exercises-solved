f852235ccb31ea232bd1063463ac98d46ca532e8
paulotijero <paulotijero@gmail.com>
2019-03-13 00:24:20 -0500

    Add test to caesar-cipher-helper problem

---
+++ caesar-cipher-helper/caesar_rspec.rb
@@ -0,0 +1,37 @@
+require './caesar'
+
+RSpec.describe "Test encrypt" do
+  c = CaesarCipher.new(5)
+
+  # Test.assert_equals(c.encode('Codewars'), 'HTIJBFWX')
+  # Test.assert_equals(c.decode('HTIJBFWX'), 'CODEWARS')
+  # Test.assert_equals(c.encode('WAFFLES'), 'BFKKQJX')
+  # Test.assert_equals(c.decode('BFKKQJX'), 'WAFFLES')
+  # Test.assert_equals(c.encode("IT'S A SHIFT CIPHER!!"), "NY'X F XMNKY HNUMJW!!")
+  # Test.assert_equals(c.decode("NY'X F XMNKY HNUMJW!!"), "IT'S A SHIFT CIPHER!!")
+  
+  it "Encode the string 'Codewars'" do
+    expect(c.encode("Codewars")).to eq("HTIJBFWX")
+  end
+
+  it "Encode the string 'WAFFLES'" do
+    expect(c.encode("WAFFLES")).to eq("BFKKQJX")
+  end
+
+  it "Encode the string 'IT'S A SHIFT CIPHER!!'" do
+    expect(c.encode("IT'S A SHIFT CIPHER!!")).to eq("NY'X F XMNKY HNUMJW!!")
+  end
+
+  it "Decode the string 'HTIJBFWX'" do
+    expect(c.decode("HTIJBFWX")).to eq("CODEWARS")
+  end
+
+  it "Decode the string 'BFKKQJX'" do
+    expect(c.decode("BFKKQJX")).to eq("WAFFLES")
+  end
+
+  it "Decode the string 'NY'X F XMNKY HNUMJW!!'" do
+    expect(c.decode("NY'X F XMNKY HNUMJW!!")).to eq("IT'S A SHIFT CIPHER!!")
+  end
+
+end
