4cb0f29e0dd61bfa4ffef48878ed59900e49e785
paulotijero <paulotijero@gmail.com>
2019-03-13 12:20:32 -0500

    Add solution to Climbing Stairs problem

---
+++ climbing-stairs/steps.rb
@@ -0,0 +1,27 @@
+# Ingrese un numero para la sucesion de fibonacci: 10
+# |	Valores iniciales	| = |	nuevo valor de Tmp	|
+# |	Fib		Tmp	|   |	  tmp = tmp + fib       |
+# |	[0]	+	[1]	| = |		[1]		|
+# |	[1]	+	[1]	| = |		[2]		|
+# |	[1]	+	[2]	| = |		[3]		|
+# |	[2]	+	[3]	| = |		[5]		|
+# |	[3]	+	[5]	| = |		[8]		|
+# |	[5]	+	[8]	| = |		[13]		|
+# |	[8]	+	[13]	| = |		[21]		|
+# |	[13]	+	[21]	| = |		[34]		|
+# |	[21]	+	[34]	| = |		[55]		|
+# |	[34]	+	[55]	| = |		[89]		|
+
+def climb_stairs(n)
+  tmp, fib = 1, 0
+  if n > 0
+    (1..n).each {
+      tmp = tmp + fib
+      fib = tmp - fib
+    }
+    puts tmp
+    return tmp
+  end
+end 
+
+climb_stairs(10)
